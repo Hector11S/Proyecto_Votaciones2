4306ebfdb30d28d1f5014b5212dcfb82d0b3d35a
Victor Espinoza <vespinozabenitez@gmail.com>
2024-04-02 15:31:49 -0600

    aaaaaaaaaaaaaa
    aaaaaaaaaaaaaa

---
+++ Frontend_Sistema_Votaciones/Frontend_Sistema_Votaciones/Models/DepartamentoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend_Sistema_Votaciones.Models
+{
+    public class DepartamentoViewModel
+    {
+        public string Dept_Codigo { get; set; }
+        public string Dept_Descripcion { get; set; }
+    }    
+}
