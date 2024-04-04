ab37819c1f018689ad19e1d595eea5bad24a7fb5
Hector11S <161340531+Hector11S@users.noreply.github.com>
2024-04-03 20:55:44 -0600

    j

---
+++ Frontend_Sistema_Votaciones/Frontend_Sistema_Votaciones/Controllers/DepartamentoController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Frontend_Sistema_Votaciones.Servicios;
+using Frontend_Sistema_Votaciones.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Frontend_Sistema_Votaciones.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public DepartamentoServicios _departamentoServicios;
+        public DepartamentoController(DepartamentoServicios departamentoServicios)
+        {
+            _departamentoServicios = departamentoServicios;
+        }
+        // GET: DepartamentosController
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var model = new List<DepartamentoViewModel>();
+                var list = await _departamentoServicios.ObtenerDepartamentoList();
+                return View(list.Data);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // GET: Departamento/Details/5
+        [HttpGet("Departamento/Details/{Dept_Codigo}")]
+        public async Task<IActionResult> Details(string Dept_Codigo)
+        {
+            try
+            {
+                var model = new DepartamentoViewModel();
+                var list = await _departamentoServicios.ObtenerDepartamento(Dept_Codigo);
+                return View(list.Data);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: DepartamentosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DepartamentosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(DepartamentoViewModel item)
+        {
+            try
+            {
+                item.Dept_UsuarioCreacion = 2;
+                item.Dept_FechaCreacion = DateTime.Now;
+                var list = await _departamentoServicios.CrearDepartamento(item);
+                return RedirectToAction("Index");
+                //return View(new List<DepartamentoViewModel> { (DepartamentoViewModel)list.Data } );
+            }
+            catch (Exception ex)
+            {
+                return View(item);
+            }
+        }
+
+        // GET: Departamento/Edit/5
+        [HttpGet("Departamento/Edit/{Dept_Codigo}")]
+        public async Task<IActionResult> Edit(string Dept_Codigo)
+        {
+            try
+            {
+                var model = await _departamentoServicios.ObtenerDepartamento(Dept_Codigo);
+                return Json(model.Data);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+
+
+        // POST: DepartamentosController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //GET: DepartamentosController/Delete/5
+        //public ActionResult Delete()
+        //{
+        //    try
+        //    {
+        //        return View();
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        return RedirectToAction("Index");
+        //    }
+        //}
+
+
+        // POST: DepartamentosController/DeleteConfirmed
+        [HttpPost("/Departamento/DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed([FromForm] string Dept_Codigo)
+        {
+            try
+            {
+           
+                var result = await _departamentoServicios.EliminarDepartamento(Dept_Codigo);
+                if (result.Success)
+                {
+              
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+         
+                    return View("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+        
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Frontend_Sistema_Votaciones/Frontend_Sistema_Votaciones/Servicios/DepartamentoServicios.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Frontend_Sistema_Votaciones.Models;
+using Frontend_Sistema_Votaciones.WebAPI;
+
+namespace Frontend_Sistema_Votaciones.Servicios
+{
+    public class DepartamentoServicios
+    {
+        private readonly API _api;
+
+        public DepartamentoServicios(API api)
+        {
+            _api = api;
+        }
+        public async Task<ServiceResult> ObtenerDepartamentoList()
+        {
+            var result = new ServiceResult();
+            try
+            {
+                var response = await _api.Get<IEnumerable<DepartamentoViewModel>, IEnumerable<DepartamentoViewModel>>(req =>
+                {
+                    req.Path = $"API/Departamento/List";
+                });
+                if (!response.Success)
+                {
+                    return result.FromApi(response);
+                }
+                else
+                {
+                    return result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+        public async Task<ServiceResult> ObtenerDepartamento(string Dept_Codigo)
+        {
+            var result = new ServiceResult();
+            try
+            {
+                var response = await _api.Get<IEnumerable<DepartamentoViewModel>, DepartamentoViewModel>(req =>
+                {
+                    req.Path = $"API/Departamento/Fill?Dept_Codigo={Dept_Codigo}";
+                });
+                if (!response.Success)
+                {
+                    return result.FromApi(response);
+                }
+                else
+                {
+                    return result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+        public async Task<ServiceResult> CrearDepartamento(DepartamentoViewModel item)
+        {
+            var result = new ServiceResult();
+            try
+            {
+                var response = await _api.Post<DepartamentoViewModel, ServiceResult>(req =>
+                {
+                    req.Path = $"API/Departamento/Insert";
+                    req.Content = item;
+                });
+                if (!response.Success)
+                {
+                    return result.FromApi(response);
+                }
+                else
+                {
+                    return result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+        public async Task<ServiceResult> EliminarDepartamento(string Dept_Codigo)
+        {
+            var result = new ServiceResult();
+            try
+            {
+                var response = await _api.Delete<string, ServiceResult>(req =>
+                {
+                    req.Path = $"API/Departamento/Delete?Dept_Codigo={Dept_Codigo}";
+                });
+
+                if (response.Success)
+                {
+                    return result.Ok(response.Data);
+                }
+                else
+                {
+                    return result.FromApi(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                return result.Error(Helpers.GetMessage(ex));
+            }
+        }
+
+
+
+    }
+}
