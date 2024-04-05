c8dbebb13873e7ef83f37329f2fb355b349d9490
Hector11S <161340531+Hector11S@users.noreply.github.com>
2024-04-05 04:46:39 -0600

    bbbb

--- Frontend_Sistema_Votaciones/Frontend_Sistema_Votaciones/Controllers/HomeController.cs
+++ Frontend_Sistema_Votaciones/Frontend_Sistema_Votaciones/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Frontend_Sistema_Votaciones.Models;
+using Frontend_Sistema_Votaciones.Servicios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,12 +13,40 @@ namespace Frontend_Sistema_Votaciones.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly VotanteServicios _votanteServicios;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, VotanteServicios votanteServicios)
         {
             _logger = logger;
+            _votanteServicios = votanteServicios;
         }
-
+        [HttpGet("[controller]/ObtenerVotantePorDNI")]
+        public async Task<IActionResult> ObtenerVotantePorDNI(string Vota_DNI)
+        {
+            try
+            {
+                var response = await _votanteServicios.ObtenerVotantePorDNI(Vota_DNI);
+                if (response.Data == null)
+                {
+                    TempData["Advertencia"] = response.Message;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    var votante = (VotanteViewModel)response.Data;
+                    if (votante.Vota_YaVoto == true)
+                    {
+                        TempData["Advertencia"] = "Usted ya votó";
+                        return RedirectToAction("Index");
+                    }
+                    return RedirectToAction("Create", "VotosPorMesa");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json("Error de capa 8");
+            }
+        }
         public IActionResult Index()
         {
             return View();
--- Sistema_Votaciones/Sistema_Votaciones.Entities/Entities/tbAlcaldes.cs
+++ Sistema_Votaciones/Sistema_Votaciones.Entities/Entities/tbAlcaldes.cs
@@ -8,9 +8,12 @@ namespace Sistema_Votaciones.Entities.Entities
 {
     public partial class tbAlcaldes
     {
+        public string Nombre_Completo { get; set; }
         public string Alca_Imagen { get; set; }
         public string Muni_Codigo { get; set; }
+        public string Muni_Descripcion { get; set; }
         public int Part_Id { get; set; }
+        public string Part_Descripcion { get; set; }
         public int Alca_UsuarioCreacion { get; set; }
         public DateTime Alca_FechaCreacion { get; set; }
         public int? Alca_UsuarioModifica { get; set; }
